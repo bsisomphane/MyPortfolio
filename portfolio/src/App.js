dbfe858141268dd3c0228a8b50784c92a097dbba
Bailey Sisomphane <bsisomphane7@gmail.com>
2022-02-03 23:12:07 -0600

    added Services component

--- portfolio/src/App.js
+++ portfolio/src/App.js
@@ -1,12 +1,14 @@
 import About from './About'
 import './App.css';
 import Home from './Home';
+import Service from './Service';
 
 function App() {
   return (
     <div className="App">
      <Home />
      <About />
+     <Service />
     </div>
   );
 }
